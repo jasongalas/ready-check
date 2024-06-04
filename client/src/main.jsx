7a2a76fb7b727f5bb9303313402d508fd55c37d9
[chase friedman] <[chaseafriedman@gmail.com]>
2024-06-04 15:35:15 -0700

    fixed css and homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,45 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import CreateReadyCheckPage from './pages/Create';
-import LiveReadyCheck from './pages/LiveReadyCheck';
+// import LiveReadyCheck from './pages/LiveReadyCheck';
 import Home from './pages/Home';
-import LoginPage from './pages/Login';
-import SignUpPage from './pages/Signup';
+// import LoginPage from './pages/Login';
+// import SignUpPage from './pages/Signup';
+// import ProfilePage from './pages/ProfilePage.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>,
+            },
+            // {
+            //     path: '/login',
+            //     element: <LoginPage/>,
+            // },
+            // {
+            //     path: '/signup',
+            //     element: <SignUpPage/>,
+            // },
+            // {
+            //     path: `/readycheck/:id`,
+            //     element: <LiveReadyCheck/>,
+            // },
+            // {
+            //     path: '/profile/:id',
+            //     element: <ProfilePage/>
+            // },
+            // {
+            //     path: '/myprofile',
+            //     element: <ProfilePage/>
+            // }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
+    <RouterProvider router = {router}/>
 )
