ba8ff86d6f0b905390a597dd7f25f18008584fdf
Tyrion Lannister <win.pomerantz@gmail.com>
2024-06-05 13:14:05 -0700

    midway

--- server/server.js
+++ server/server.js
@@ -2,39 +2,55 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const http = require('http');
 
+const { Server } = require('socket.io');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
-
 const PORT = process.env.PORT || 3001;
+
 const app = express();
-const server = new ApolloServer({
+const server = http.createServer(app);
+const io = new Server(server);
+const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
 const startApolloServer = async () => {
-  await server.start();
-  
+  await apolloServer.start();
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  
-  app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
-
+  app.use('/graphql', expressMiddleware(apolloServer, { context: authMiddleware }));
   // if we're in production, serve client/dist as static assets
+
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
-  } 
+  }
 
   db.once('open', () => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+
+  // Socket.io connection
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle socket events here
+    socket.on('message', (msg) => {
+      console.log('Message received:', msg);
+      io.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
     });
   });
 };
